5b56d71e4fdbfb43ecfc8312664401ebca06f798
Erickson <eagramonte@intellisys.com.do>
2024-01-08 21:07:17 -0400

    first commit

---
+++ lang/es/blog.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'containing' => 'Contenedor',
+
+    'popular' => 'Popular',
+    'latest' => 'Ultimo',
+    'oldest' => 'Mas viejo',
+
+    'search' => 'Buscar',
+    'search_placeholder' => 'Buscando ...',
+    'recommended_topics' => 'Temas recomendados',
+    'min_read' => 'Min'
+];
---
+++ lang/es/home.php
@@ -0,0 +1,15 @@
+<?php
+
+
+return [
+
+    'featured_posts' => 'Entradas destacadas',
+    'latest_posts' => 'Últimos posts',
+    'more_posts' => 'Mas Posts',
+
+    'hero' => [
+        'title' => 'Bienvenido a',
+        'desc' => 'Mantente al tanto de todas las informaciones',
+        'cta' => 'Iniciar lectura'
+    ]
+];
